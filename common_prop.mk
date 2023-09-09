f780113ce06c76d589504573c7f5d0db0be923a7
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-09-09 12:08:53 +0600

    sdm660-common : remove better ram props

--- common_prop.mk
+++ common_prop.mk
@@ -29,18 +29,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     iorapd.readahead.enable=false \
     persist.device_config.runtime_native_boot.iorap_readahead_enable=false
 
-# Better Ram
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.fha_enable=true \
-    ro.sys.fw.bg_apps_limit=32 \
-    ro.config.dha_cached_max=16 \
-    ro.config.dha_empty_max=42 \
-    ro.config.dha_empty_init=32 \
-    ro.config.dha_lmk_scale=0.545 \
-    ro.config.dha_th_rate=2.3 \
-    ro.config.sdha_apps_bg_max=64 \
-    ro.config.sdha_apps_bg_min=8
-
 # Blur
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.disable_blurs=1 \
